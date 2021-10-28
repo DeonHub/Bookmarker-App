117830d0980c1e475cf8845f044bef62a229c6cd
Gideon Impraim <82456010+GI-10836535@users.noreply.github.com>
2021-10-28 23:01:20 +0000

    Update main.js

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ function fetchBookmarks() {
     	bookmarkList.innerHTML += 
                                   '<tr>'+
                                   '<td><span style="font-size:15px;text-transform:capitalize;">'+name+'</span></td>'+
-                                  '<td><a class ="btn btn-primary btn-sm"  target="_blank" href="'+url+'">Visit Site</a></td>'+
+                                  '<td><a class ="btn btn-primary btn-sm"  target="_blank" href="'url'">Visit Site</a></td>'+
                                   '<td><a onclick="deleteBookmark(\''+url+'\')" class ="btn btn-danger btn-sm" href="#">Delete</a></td>'+
                                   '</tr>'
 
